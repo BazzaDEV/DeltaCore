12a09828fbd131c2284a00caf27d85f6b50e86cd
Bazza <bazarjani.kian@gmail.com>
2020-07-04 15:46:47 -0400

    Re-added View Player Inventory and added View Ender Chest to PlayerActionsInventory

--- src/main/java/me/bazzadev/deltacore/listeners/InventoryClickListener.java
+++ src/main/java/me/bazzadev/deltacore/listeners/InventoryClickListener.java
@@ -1,5 +1,6 @@
 package me.bazzadev.deltacore.listeners;
 
+import me.bazzadev.deltacore.managers.VanishManager;
 import me.bazzadev.deltacore.staffmode.PlayerActionsInventory;
 import me.bazzadev.deltacore.managers.StaffGUIManager;
 import me.bazzadev.deltacore.utilities.ChatUtil;
@@ -17,9 +18,11 @@ import java.util.UUID;
 public class InventoryClickListener implements Listener {
 
     private final StaffGUIManager staffGUIManager;
+    private final VanishManager vanishManager;
 
-    public InventoryClickListener(StaffGUIManager staffGUIManager) {
+    public InventoryClickListener(StaffGUIManager staffGUIManager, VanishManager vanishManager) {
         this.staffGUIManager = staffGUIManager;
+        this.vanishManager = vanishManager;
     }
 
     @EventHandler
@@ -41,16 +44,30 @@ public class InventoryClickListener implements Listener {
         if (event.getView().getTitle().startsWith(PlayerActionsInventory.INV_TITLE)) {
 
             event.setCancelled(true);
+            targetPlayer = PlayerActionsInventory.getPlayerFromGUIHead(player);
 
             if (itemClicked.equals(Vars.GO_BACK)) {
+                // Player wants to go back to the staff GUI
                 player.openInventory(staffGUIManager.getMainGUI());
 
             } else if (itemClicked.getType().equals(Material.CHEST)) {
-                // targetPlayer = PlayerActionsInventory.getPlayerFromGUIHead(player);
-                // player.openInventory(targetPlayer.getInventory());
+                // Player wants to view the target player's inventory contents
+                player.openInventory(targetPlayer.getInventory());
+
+            } else if (itemClicked.getType().equals(Material.ENDER_CHEST)) {
+                // Player wants to view the target player's ender chest contents
+                player.openInventory(targetPlayer.getEnderChest());
+
 
             } else if (itemClicked.getType().equals(Material.ENDER_PEARL)) {
-                targetPlayer = PlayerActionsInventory.getPlayerFromGUIHead(player);
+                // Player wants to teleport to the target player
+
+                // Vanish the player if they haven't already done so
+                if ( !VanishManager.isVanished(player) ) {
+                    vanishManager.toggle(player);
+                }
+
+                // Teleport the player
                 player.teleport(targetPlayer.getLocation());
                 player.sendMessage(ChatUtil.color(Vars.PLUGIN_PREFIX + "&7You have been teleported to " + ChatUtil.playerNameWithPrefix(targetPlayer) + "&7's location."));
 
