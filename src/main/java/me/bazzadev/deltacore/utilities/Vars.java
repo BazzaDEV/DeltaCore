90dd3409f29e62eb771b80568dc7819abf8b41a0
Bazza <bazarjani.kian@gmail.com>
2020-07-06 03:01:47 -0400

    Removed unused prefix constants and updated Vanish messages prefix

--- src/main/java/me/bazzadev/deltacore/utilities/Vars.java
+++ src/main/java/me/bazzadev/deltacore/utilities/Vars.java
@@ -1,18 +1,15 @@
 package me.bazzadev.deltacore.utilities;
 
-import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 public class Vars {
 
-    public static final String PLUGIN_PREFIX = ChatColor.translateAlternateColorCodes('&', "&8&l" + '▍' + " &c&lDelta&f&lCore &8▎ &r");
-    public static final String NO_PERMISSION = Vars.PLUGIN_PREFIX + ChatColor.RED + "No permission, bud.";
+    public static final String PLUGIN_PREFIX = ChatUtil.color("&8&l" + '▍' + " &c&lDelta&f&lCore &8▎ &r");
     public static final String ERROR_PREFIX = ChatUtil.color("&8&l" + '▍' + " &4&lERROR &8▎ &r");
     public static final String VANISH_PREFIX = ChatUtil.color("&dVanish &8▎ &r");
 
-    public static final String PLAYER_AFK_PREFIX = ChatUtil.color("&f[&7AFK&f] ");
-    public static final String PLAYER_STAFFMODE_PREFIX = ChatUtil.color("&f[&bStaff&f] ");
+    public static final String NO_PERMISSION = ChatUtil.color(PLUGIN_PREFIX + "&cNo permission, bud.");
 
     public static final String SHULKER_BOX_SILENT_MODE_TITLE = ChatUtil.color("&fShulker Box &8[&7Silent Mode&8]");
     public static final String CHEST_SILENT_MODE_TITLE = ChatUtil.color("&fChest &8[&7Silent Mode&8]");
