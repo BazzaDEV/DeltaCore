befabf118e731d0caba1ad853931c0563bb34805
BuildTools <unconfigured@null.spigotmc.org>
2021-06-02 00:06:38 -0400

    Forgto files

---
+++ src/main/java/dev/bazza/deltacore/DeltaCore.java
@@ -0,0 +1,17 @@
+package dev.bazza.deltacore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeltaCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
